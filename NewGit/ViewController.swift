3f770d7c41b9cdeb4ddaecde9ddb572457a2956a
vinodkumarch <Ramyaanuvvula@gmail.com>
2019-01-24 17:47:11 +0530

    First Commit

---
+++ NewGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewGit
+//
+//  Created by MXMACMINI1 on 24/01/19.
+//  Copyright © 2019 MB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
